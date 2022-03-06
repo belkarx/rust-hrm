bded182e8af7216a20907947b3b6999a81a422c3
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-06 14:39:09 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-use std::fs;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u8,
+    address: Address,
+    phone_numbers: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Address {
+    street: String,
+    city: String,
+    country: String,
+}
 
 fn main() {
-  let data = fs::read_to_string("/etc/hosts").expect("Unable to read file");   
+    let mut file = File::open("text.json").unwrap();
+
+    let person: Person = serde_json::from_str(file).expect("JSON was not well-formatted");
+    println!("{:?}", person)
 }
