af9b4724308976e7edbb2af58494e8bee4f61f39
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 14:25:30 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,14 @@ fn main() {
     //let mut book_reviews:HashMap<String, String> = HashMap::new();
 }
 
+fn show_next(s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(Dialog::text("Did you do the thing?")
+        .title("Question 1")
+        .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
+        .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
+        .button("Uh?", |s| s.add_layer(Dialog::info("Try again!"))));
+}
 
 #[cfg(test)]
 mod tests {
