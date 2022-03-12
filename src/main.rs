8ab87d54ea693486a80922cfa171afffb1a0b509
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 10:22:32 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,7 @@ fn init_person() {
                 None
             }
         },
-        uses: {
-            let uses: Vec<String> = get_string("Uses (comma separated): ").split(", ").map(|s| s.to_string()).collect();
-            uses
-        },
+        uses: get_string("Uses (comma separated): ").split(", ").map(|s| s.to_string()).collect();
         skill: get_string("Technical skill from 0 to 5: ").trim().parse::<i16>().unwrap(), //may be removed
         social: get_string("Social usefulness from 0 to 5: ").trim().parse::<i16>().unwrap(),
         source: get_string("Where did you find this person (IRL, Discord, Reddit, School, etc): ")
