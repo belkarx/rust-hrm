ff760d6492a99766f0980c4a319a600fa9ca1b67
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 08:54:27 -0800

    Auto-commit: saved src/utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -4,11 +4,11 @@ pub fn get_string(prompt: &'static str) -> String {
     print!("{}", prompt);
     let _=stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
-    if let Some('\n')=s.chars().next_back() {
+    /*if let Some('\n')=s.chars().next_back() {
         s.pop();
     }
     if let Some('\r')=s.chars().next_back() {
         s.pop();
-    }
-    return s;
+    }*/
+   s.pop();
 }
