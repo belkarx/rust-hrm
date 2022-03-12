4f8f88423aa86a075d6452a796d7553a841c2ec7
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 08:30:44 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::collections::HashMap;
 mod utils;
-use crate::utils::utils::get_subject_info;
 
 fn main() {
-    utils::get_subject_info();
+    get_subject_info();
     //let mut book_reviews:HashMap<String, String> = HashMap::new();
 }
 
