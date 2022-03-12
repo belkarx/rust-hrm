fc4c624cf676436280cabbc01c6897a5e5957692
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 08:40:58 -0800

    Auto-commit: saved src/utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 pub fn get_string(prompt: &str) -> String {
     use std::io::{stdin,stdout,Write};
     let mut s = String::new();
-    print!("Alias: ");
+    print!(prompt);
     let _=stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
     if let Some('\n')=s.chars().next_back() {
