98cee0ae31183c48b2d1d637f45816d6534462dc
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 10:50:17 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn init_person() -> Person {
         name: Some(get_string("Name [optional]: ")).filter(|s| s.len() > 0),
         main_contact: get_string("Main contact: "),
         other_contacts: {
-            let other_contacts:<Vec<String> = get_string("Other contacts (comma separated) [optional]: ").split(", ").map(|s| s.to_string()).collect();
+            let other_contacts:<Vec<String>> = get_string("Other contacts (comma separated) [optional]: ").split(", ").map(|s| s.to_string()).collect();
             other_contacts.filter(|s| s.len() > 0)
             /*if !other_contacts.is_empty() {
                 Some(other_contacts)
