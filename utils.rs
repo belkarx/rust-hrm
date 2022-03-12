72f77466a3d63bc24371c703ce078afbde196d6c
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 08:21:54 -0800

    Auto-commit: saved utils.rs

--- utils.rs
+++ utils.rs
@@ -1,17 +1,17 @@
 mod utils {
     fn get_subject_info() {
-    use std::io::{stdin,stdout,Write};
-    let mut s = String::new();
-    print!("Alias: ");
-    let _=stdout().flush();
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
-    if let Some('\n')=s.chars().next_back() {
-        s.pop();
+        use std::io::{stdin,stdout,Write};
+        let mut s = String::new();
+        print!("Alias: ");
+        let _=stdout().flush();
+        stdin().read_line(&mut s).expect("Did not enter a correct string");
+        if let Some('\n')=s.chars().next_back() {
+            s.pop();
+        }
+        if let Some('\r')=s.chars().next_back() {
+            s.pop();
+        }
+        println!("You typed: {}",s);
     }
-    if let Some('\r')=s.chars().next_back() {
-        s.pop();
-    }
-    println!("You typed: {}",s);
-}
 }
 
