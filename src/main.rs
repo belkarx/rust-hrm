091cc1b3688e6485b98ca0b8239b55eb87fa6f33
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 08:44:18 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod utils;
 
 fn main() {
     let alias = utils::get_string("Alias: ");
-    let contact = utils::get_string("Contact info (comma separated): ");
+    let contact = utils::get_string("Contact info (comma separated): ").split(", ").collect();
+    let contacts: Vec<&str> = split.collect();
     println!("{}", alias);
     //let mut book_reviews:HashMap<String, String> = HashMap::new();
 }
