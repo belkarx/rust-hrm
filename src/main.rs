780f3399890b68f0a350113b910c07d8abd02b3c
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 08:52:23 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use utils::get_string;
 fn main() {
     let alias = get_string("Alias: ");
     
-    let contact_raw = get_string("Contact info (comma separated): ").split(", ").collect();
+    let contact_raw = get_string("Contact info (comma separated): ");
     let contacts:Vec<&str> = contact_raw.split(", ").collect();
 
     let uses_raw = get_string("Uses (comma separated): ");
