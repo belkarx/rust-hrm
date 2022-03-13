c05e2fbc05788742923613d722c73724f51fe625
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 22:46:24 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ fn read_from_file_as_hashmap() -> HashMap<String, Person> {
 
 fn main() {
     //let mut vec = read_from_file();
-    let mut hm: HashMap<String, Person> = read_from_file_as_hashmap();
+    let mut hm: HashMap<String, Person> = HashMap::new();//read_from_file_as_hashmap();
 
     loop {
         let person = init_person();
