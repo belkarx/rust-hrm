77cb5b78bab601f276fa4ccb387a161cad4ef4de
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-13 15:53:04 -0700

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -123,7 +123,7 @@ fn main() {
     let field = get_string("choose a (TEXT) field to alter: "); 
     let value = get_string("what value are you changing it to: ");
         match &field[..] {
-            //"alias" => p.alias = value, //alters hasmap key
+            //"alias" => p.alias = value, //alters hashmap key
             "" => p.name = value,
             _ => panic!("Lol field doesn't exist /shrug")
         };
