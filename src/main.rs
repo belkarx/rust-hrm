48b0d86337f2e9c9405a6c17a7bd91d5fb902da2
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-13 16:50:26 -0700

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,6 @@ impl Person {
         let mut added_contacts: Vec<String> = get_string("Contacts to add: ").split(", ").map(|s| s.to_string()).collect();
         if self.other_contacts != None {
             self.other_contacts.unwrap().append(&mut added_contacts);
-            self.other_contacts = Some(total);
         } else {
             self.other_contacts = Some(added_contacts);
         }
