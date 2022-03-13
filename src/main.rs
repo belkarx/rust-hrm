36b499cd1d98fe6e3ea7a360a6b4749c582c6e42
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-13 15:26:40 -0700

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -86,8 +86,6 @@ fn main() {
     let mut hm: HashMap<String, Person> = read_from_file_as_hashmap();
     let chosen = by_alias(&hm);
     let p = hm.get_mut(&chosen).unwrap();
-    by_field(hm.get_mut(&chosen).unwrap());
-    (*).alias = ;
     println!("{:#?}", p);
         let field = get_string("choose a field to alter: "); 
         let value = get_string("what value are you changing it to: ");
