ead64ceaef85f5153c1a07718c0ab6f91b108f1e
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-13 15:55:02 -0700

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -121,8 +121,8 @@ fn main() {
     let p = hm.get_mut(&chosen).unwrap();
     println!("{:#?}", p);
     let field = get_string("choose a (TEXT) field to alter: "); 
-    /*let value = get_string("what value are you changing it to: ");
-        match &field[..] {
+    let value = get_string("what value are you changing it to: ");
+    /*    match &field[..] {
             //"alias" => p.alias = value, //alters hashmap key
             "name" => p.name = value,
             "" => p.name = value,
