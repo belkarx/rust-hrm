74b980a98bcff9d3b311a9f38ca5ff8fc33e1282
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-12 22:03:20 -0800

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn write_to_file(p: Person) {
 }
 
 fn read_from_file() {
-    println!("{:#?}", serde_json::from_str(&fs::read_to_string("address.txt").unwrap()).unwrap());
+    let data: Vec<Person> = serde_json::from_str(&fs::read_to_string("address.txt").unwrap()).unwrap();
 }
 
 fn main() {
