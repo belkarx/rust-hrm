906570846bb7de8bfe30342daa2ea1203efaa6f1
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-13 16:19:03 -0700

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use serde::{Serialize, Deserialize};
 use std::fs;
 use serde_json::Map;
 
-#[derive(Debug, Serialize, Deserialize, Clone, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
 struct Person {
     alias: String,
     name: Option<String>, //optional
