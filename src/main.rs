600e713a71abafe950d3ed06d0ef84eb8b6381ca
belkarx <84473367+belkarx@users.noreply.github.com>
2022-03-13 17:42:45 -0700

    Auto-commit: saved main.rs

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ impl Person {
         }
     }
     fn add_uses(&mut self) {
-        let mut added: vec<string> = get_string("uses to add: ").split(", ").map(|s| s.to_string()).collect();
+        let mut added: Vec<String> = get_string("uses to add: ").split(", ").map(|s| s.to_string()).collect();
         self.uses.append(&mut added);
     }
     fn del_uses(&mut self) {
